14aa8880da5620c1e070ed4ca3b5e66f1f75ade8
Nihal <91502873+exosauce@users.noreply.github.com>
2022-06-06 22:38:00 -0400

    Add files via upload

---
+++ class_1.py
@@ -0,0 +1,17 @@
+
+'''
+def area(r):
+    pi=3.145
+    k = 205
+    area=2*pi*r*r
+    return area
+
+
+x = area(8)
+'''
+def area(l,w):
+    area=l*w
+    return area
+
+print("Area with 1=2 and 2=4 is " + str(area(2,4)))
+print("Area"*2)
